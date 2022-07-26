c3f0716526e336f4dc547d9a7be42a1a0e4c8f34
Sigita Gavare <gavare33@gmail.com>
2022-07-26 21:18:54 +0300

    JavaScript

---
+++ JavaScript/script.js
@@ -0,0 +1,18 @@
+let Name = prompt("Enter your first name:", "Name");
+let LastName = prompt("Enter your last name:", "Last name");
+alert("Welcome "+ Name + " "+ LastName + "!")
+
+let x = prompt("Enter the value of first number:", "0");
+let y = prompt("Enter the value of second number:", "0");
+alert("First number ="+ x);
+alert("Second number ="+ y);
+var result = Number.parseInt(x) + Number.parseInt(y);
+alert("The sum of numbers ="+ result);
+var result = Number.parseInt(x) - Number.parseInt(y);
+alert("The subtraction of numbers ="+ result);
+var result = Number.parseInt(x) * Number.parseInt(y);
+alert("The multiplication of numbers ="+ result);
+var result = Number.parseInt(x) / Number.parseInt(y);
+alert("The division of numbers ="+ result);
+var result = Number.parseInt(x) % Number.parseInt(y);
+alert("The modulus of numbers ="+ result);
