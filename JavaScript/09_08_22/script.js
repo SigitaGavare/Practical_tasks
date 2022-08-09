499448e576c8f709527000a7e1bd60ff8ccaea88
Sigita Gavare <gavare33@gmail.com>
2022-08-09 21:39:48 +0300

    DOM

---
+++ JavaScript/09_08_22/script.js
@@ -0,0 +1,42 @@
+function summ(){
+    const firstNum = document.getElementById("firstNumber").value;
+    const secondNum = document.getElementById("secondNumber").value;
+    const result = Number(firstNum) + Number(secondNum);
+    const resultElement = document.getElementById("resultbut");
+    resultElement.innerText= "The result is = " + result;
+    resultElement.style.border = "1px solid black";
+};
+
+
+function Subtraction(){
+    const firstNum = document.getElementById("firstNumber").value;
+    const secondNum = document.getElementById("secondNumber").value;
+    const result = Number(firstNum) - Number(secondNum);
+    const resultElement = document.getElementById("resultbut");
+    resultElement.innerText= "The result is = " + result;
+    resultElement.style.border = "1px solid black";
+};
+function Multiplication(){
+    const firstNum = document.getElementById("firstNumber").value;
+    const secondNum = document.getElementById("secondNumber").value;
+    const result = Number(firstNum) * Number(secondNum);
+    const resultElement = document.getElementById("resultbut");
+    resultElement.innerText= "The result is = " + result;
+    resultElement.style.border = "1px solid black";
+};
+function Division(){
+    const firstNum = document.getElementById("firstNumber").value;
+    const secondNum = document.getElementById("secondNumber").value;
+    const result = Number(firstNum) / Number(secondNum);
+    const resultElement = document.getElementById("resultbut");
+    resultElement.innerText= "The result is = " + result;
+    resultElement.style.border = "1px solid black";
+};
+function Modulus(){
+    const firstNum = document.getElementById("firstNumber").value;
+    const secondNum = document.getElementById("secondNumber").value;
+    const result = Number(firstNum) % Number(secondNum);
+    const resultElement = document.getElementById("resultbut");
+    resultElement.innerText= "The result is = " + result;
+    resultElement.style.border = "1px solid black";
+};
