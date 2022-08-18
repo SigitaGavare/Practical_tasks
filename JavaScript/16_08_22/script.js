bc92ef5d2c21428ad76b44b05ad4baa7af140365
Sigita Gavare <gavare33@gmail.com>
2022-08-18 14:18:59 +0300

    Čalculator

--- JavaScript/16_08_22/script.js
+++ JavaScript/16_08_22/script.js
@@ -1,86 +1,127 @@
-//(Math.sqrt())
-$("#percent").on("click",function(){
-$("#result").innerText = 0.01 * Number($("#result".innerText));
-});
-$("#delAll").on("click",function(){
-$("result")=0
-});
-$("#delThis").on("click",function(){
+var x;
+var y;
+let rez;
+function percent(){
+x =Number(document.getElementById("result").innerText);
+x*= 0.01;
+document.getElementById("result").innerText = x
+};
+function delAll(){
+    document.getElementById("result").innerText = 0;
+    y=0
+};
+function delThis(){
+    document.getElementById("result").innerText = 0
+};
+function del(){    
+    x=document.getElementById("result").innerText;
+    var m=x.substring(0, x.length - 1);
+    document.getElementById("result").innerText = m
+};
 
-});
-$("#del").on("click",function(){
+function reverse(){
+    x = Number(document.getElementById("result").innerText);
+    x = 1/x;
+    document.getElementById("result").innerText = x
+};
 
-});
-$("#reverse").on("click",function(){
+function sqr(){
+    x = Number(document.getElementById("result").innerText);
+    y = x*x;
+    document.getElementById("result").innerText = y;
+};
+function sqrt(){
+    x = Number(document.getElementById("result").innerText);
+    x = (Math.sqrt(x));
+    document.getElementById("result").innerText = x
+};
+function division(){
+    y = Number(document.getElementById("result").innerText);
+    document.getElementById("result").innerText = 0
 
-});
-$("#sqr").on("click",function(){
+};
 
-});
-$("#sqrt").on("click",function(){(Math.sqrt())
-});
-$("#division").on("click",function(){
+function sum(){
 
-});
+};
+function subtraction(){
 
-$("#sum").on("click",function(){
+};
+function multiplication(){
 
-});
-$("#subtraction").on("click",function(){
+};
 
-});
-$("#multiplication").on("click",function(){
+function minus(){
 
-});
+};
+function dot(){
 
-$("#minus").on("click",function(){
+};
+function calculate(){
+    document.getElementById("result").innerText = rez
+    y=0
+    x=0
+    rez=0
+};
 
-});
-$("#dot").on("click",function(){
-
-});
-$("#calculate").on("click",function(){
-
-});
-function addNumbers(){
-$("#1").on("click",function (){});
-$("#2").on("click",function (){});
-$("#3").on("click",function (){});
-$("#4").on("click",function (){});
-$("#5").on("click",function (){});
-$("#6").on("click",function (){});
-$("#7").on("click",function (){});
-$("#8").on("click",function (){});
-$("#9").on("click",function (){});
-$("#0").on("click",function (){});}
-
-
-
-// function addElements(){
-//     var newphone = $("#addphone").val();
-//     var newcolour = $("#addcolour").val(); 
-//     $('.table tr:last').after('<tr> <td id="newphone" class="phones">  </td> <td id="newcolour" class="colours"> </td></tr>');
-//     $("#newphone:last").text(newphone);
-//     $("#newcolour:last").text(newcolour);
-//     $("#newphone").each(function(){
-//           this.id = this.id+"1";
-//       });
-//     $("#newcolour").each(function(){
-//         this.id = this.id+"1";
-//     });
-//     $('select#phoneFilter').append('<option id="opt"></option>');
-//     $("option#opt").text(newphone);
-//     $("option#opt").each(function(){
-//         this.id = this.id+"1";
-//     });
-// }
-// function filter(){
-//     if ($("select").val() == "All"){
-//         $("td.phones").each(function (){
-//                 $(this).parent().show();})}
-//     else {$("td.phones").each(function (){
-//     if ($(this).text() !== $("select").val())
-//         $(this).parent().hide()
-//     else
-//         $(this).parent().show();
-// })}}
+function add1(){
+    x = document.getElementById("result").innerText;
+    if (x==0) {x=1}
+    else {
+    x += 1;}
+    document.getElementById("result").innerText = x}
+function add2(){
+    x = document.getElementById("result").innerText;
+    if (x==0) {x=2}
+    else {
+    x += 2;}
+    document.getElementById("result").innerText = x}
+function add3(){
+    x = document.getElementById("result").innerText;
+    if (x==0) {x=3}
+    else {
+    x += 3;}
+    document.getElementById("result").innerText = x}
+function add4(){
+    x = document.getElementById("result").innerText;
+    if (x==0) {x=4}
+    else {
+    x += 4;}
+    document.getElementById("result").innerText = x}
+function add5(){
+    x = document.getElementById("result").innerText;
+    if (x==0) {x=5}
+    else {
+    x += 5;}
+    document.getElementById("result").innerText = x}
+function add6(){
+    x = document.getElementById("result").innerText;
+    if (x==0) {x=6}
+    else {
+    x += 6;}
+    document.getElementById("result").innerText = x}
+function add7(){
+    x = document.getElementById("result").innerText;
+    if (x==0) {x=7}
+    else {
+    x += 7;}
+    document.getElementById("result").innerText = x}
+function add8(){
+    x = document.getElementById("result").innerText;
+    if (x==0) {x=8}
+    else {
+    x += 8;}
+    document.getElementById("result").innerText = x}
+function add9(){
+    x = document.getElementById("result").innerText;
+    if (x==0) {x=9}
+    else {
+    x += 9;}
+    document.getElementById("result").innerText = x}
+function add0(){
+    x = document.getElementById("result").innerText;
+    if (x==0)
+    {}
+    else {
+    x += 0}
+    document.getElementById("result").innerText = x}
