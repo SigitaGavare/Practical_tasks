fa71d20e4e19318104e2cfcadade6b3a69f1b22b
Sigita Gavare <gavare33@gmail.com>
2022-09-16 14:27:59 +0300

    ft

--- Final_task/Pages/script.js
+++ Final_task/Pages/script.js
@@ -1,45 +1,57 @@
 function desc_mara(){
     var x = document.getElementById("Mara");
+    var pic = document.getElementById("spec");
     if (x.style.display === 'none') {
         x.style.display = 'block';
+        pic.style.display = 'block';
         document.getElementById("maija").style.display = 'none';
         document.getElementById("martins").style.display = 'none';
         document.getElementById("kate").style.display = 'none';
     } else {
         x.style.display = 'none';
+        pic.style.display = 'none';
     }
 }
 function desc_maija(){
     var x = document.getElementById("maija");
+    var pic = document.getElementById("spec");
     if (x.style.display === 'none') {
         x.style.display = 'block';
+        pic.style.display = 'block';
         document.getElementById("Mara").style.display = 'none';
         document.getElementById("martins").style.display = 'none';
         document.getElementById("kate").style.display = 'none';
     } else {
         x.style.display = 'none';
+        pic.style.display = 'none';
     }
 }
 
 function desc_martins(){
     var x = document.getElementById("martins");
+    var pic = document.getElementById("spec");
     if (x.style.display === 'none') {
         x.style.display = 'block';
+        pic.style.display = 'block';
         document.getElementById("Mara").style.display = 'none';
         document.getElementById("maija").style.display = 'none';
         document.getElementById("kate").style.display = 'none';
     } else {
         x.style.display = 'none';
+        pic.style.display = 'none';
     }
 }
 function desc_kate(){
     var x = document.getElementById("kate");
+    var pic = document.getElementById("spec");
     if (x.style.display === 'none') {
         x.style.display = 'block';
+        pic.style.display = 'block';
         document.getElementById("Mara").style.display = 'none';
         document.getElementById("maija").style.display = 'none';
         document.getElementById("martins").style.display = 'none';
     } else {
         x.style.display = 'none';
+        pic.style.display = 'none';
     }
 }
